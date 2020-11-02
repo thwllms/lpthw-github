daeb44cb47b7f5ff414d5a652f4088eb48ce16d1
jpmill09 <35961262+jpmill09@users.noreply.github.com>
2020-11-02 12:34:10 -0500

    added feature goodbye
    added small function defining goodbye

--- script.py
+++ script.py
@@ -2,10 +2,20 @@
 lpthw-github
 """
 
+import sys
+
 def do_nothing():
     # this does nothing
     pass
 
+def goodbye():
+    # print goodbye world
+    print("Goodbye, World ¯\_(ツ)_/¯")
 
 if __name__ == '__main__':
     do_nothing()
+
+    script,arg = sys.argv
+
+    if arg == "goodbye":
+        goodbye()
