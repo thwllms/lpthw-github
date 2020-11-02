5f1ea7dcc24770193608c99fbb7592075c373079
Jennifer McGee <jenna.mcgee@woodplc.com>
2020-11-02 17:36:19 -0500

    added new function to get a random quote

--- script.py
+++ script.py
@@ -3,19 +3,32 @@ lpthw-github
 """
 
 import sys
+import requests
 
 
 def do_nothing():
     # this does nothing
     pass
 
+
 def goodbye():
     # print goodbye world
     print("Goodbye, World ¯\_(ツ)_/¯")
 
+
 # Printing "Hello World!"
 def print_hello():
     print("Hello World!")
+
+
+# Get a Random Quote.
+def get_quote():
+    url = "https://programming-quotes-api.herokuapp.com/quotes/random"
+    r = requests.get(url)
+    dict = r.json()
+    quote = dict['en']
+
+    print(quote)
 
 
 if __name__ == '__main__':
@@ -27,5 +40,7 @@ if __name__ == '__main__':
         print_hello()
     elif arg == "goodbye":
         goodbye()
+    elif arg == "quote":
+        get_quote()
     else:
         print("nothing")
