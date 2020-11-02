59708638ac2a97371d9508ce6616f2e204cde2da
Jennifer McGee <jenna.mcgee@woodplc.com>
2020-11-02 12:27:54 -0500

    added print_hello() function and arguments.

--- script.py
+++ script.py
@@ -2,10 +2,23 @@
 lpthw-github
 """
 
+import sys
+
+
 def do_nothing():
     # this does nothing
     pass
 
 
+# Printing "Hello World!"
+def print_hello():
+    print("Hello World!")
+
+
 if __name__ == '__main__':
     do_nothing()
+
+    script, arg = sys.argv
+
+    if arg == "hello":
+        print_hello()
